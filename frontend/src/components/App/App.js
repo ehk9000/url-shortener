41915a5699710da24271ed7b41d4a5e7c70d26b3
Erik Konner <erikkonner@gmail.com>
2021-11-04 16:23:02 -0600

    Removed default react component, created urlForm directory and file

---
+++ frontend/src/components/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import UrlForm from '../UrlForm/UrlForm';
+
+function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<h2>Url Shortener</h2>
+			</header>
+			<UrlForm />
+		</div>
+	);
+}
+
+export default App;
