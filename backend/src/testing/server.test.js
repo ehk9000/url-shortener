09727921e4f291ea043b83533f9e66c8f1c8310e
Erik Konner <erikkonner@gmail.com>
2021-11-12 15:39:00 -0700

    Tests for adding url

--- backend/src/testing/server.test.js
+++ backend/src/testing/server.test.js
@@ -2,8 +2,12 @@ import app from '../app.mjs';
 import request from 'supertest';
 import 'babel-polyfill';
 import regeneratorRuntime from 'regenerator-runtime';
+import { Urls } from '../models/urls.mjs';
 
 describe('Server', () => {
+	afterEach(async () => {
+		await Urls.deleteMany();
+	});
 	describe('/createUrl', () => {
 		it('Should validate urls', async () => {
 			const goodUrl = {
@@ -21,5 +25,21 @@ describe('Server', () => {
 			expect(response.statusCode).toBe(401);
 			expect(response.body).toEqual('Please Provide Valid URL');
 		});
+		it('Should create a new url document', async () => {
+			const fullUrlRequest = {
+				fullUrl:
+					'https://www.amazon.com/gp/product/B078BF27BF/ref=ppx_yo_dt_b_asin_title_o00_s00?ie=UTF8&psc=1',
+			};
+			const documents = await Urls.find().lean();
+
+			const response = await request(app)
+				.post('/createUrl')
+				.send(fullUrlRequest);
+			expect(response.statusCode).toBe(201);
+			expect(response.body.shortUrl).toBeTruthy();
+			expect(response.body.fullUrl).toBe(fullUrlRequest.fullUrl);
+			expect(documents.length).toEqual(0);
+		});
 	});
+	describe('/:shortUrl', () => {});
 });
